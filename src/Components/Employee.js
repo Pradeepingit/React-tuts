a269c33ec2c9dbc148db8ad2507b838864ea000f
Pradeep <pradeeps9967@gmail.com>
2025-01-18 18:31:35 +0530

    added tailwind

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,7 +1,7 @@
 function Employee(props) {
     return (
         <>
-            <h2>Employee name: {props.name}</h2>
+            <h2 className="text-3xl font-semibold">Employee name: {props.name}</h2>
             <p>Role:</p> 
             {props.role ? <p className="role">{props.role}</p> : <span className="norole">No role</span>}
 
